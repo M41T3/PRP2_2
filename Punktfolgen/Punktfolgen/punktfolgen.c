04ea05e74d33f553b6542a36b3fa7591de276194
PC-MALTE\malte <maltemueller1104@gmail.com>
2018-10-22 19:09:18 +0200

    initial commit, created "Punktfolge"-project

---
+++ Punktfolgen/Punktfolgen/punktfolgen.c
@@ -0,0 +1,22 @@
+/*
+PRP2-2 Aufgabe 2.1
+Name: Malte Müller
+Date: 18.10.2018
+*/
+
+// Range of system:
+#define X_0 -2.1
+#define X_1 0.6
+#define Y_0 -1.2
+#define Y_1 1.2
+// Step size:
+#define X_STEP 0.1
+#define Y_STEP 0.1
+// Interations;
+#define ITER 100
+
+int main(void) {
+
+
+	return 0;
+}
