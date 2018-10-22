6077687019bb1ccfd62e1c349b2106af544aa407
PC-MALTE\malte <maltemueller1104@gmail.com>
2018-10-22 21:12:15 +0200

    started to build memory allocation routine. bug in allocation (probably wrong data type for the pointers)

--- Punktfolgen/Punktfolgen/punktfolgen.c
+++ Punktfolgen/Punktfolgen/punktfolgen.c
@@ -1,8 +1,10 @@
 /*
 PRP2-2 Aufgabe 2.1
 Name: Malte Müller
-Date: 18.10.2018
+Date: 22.10.2018
 */
+
+#include <stdio.h>
 
 // Range of system:
 #define X_0 -2.1
@@ -12,11 +14,58 @@ Date: 18.10.2018
 // Step size:
 #define X_STEP 0.1
 #define Y_STEP 0.1
-// Interations;
+// Interations:
 #define ITER 100
 
 int main(void) {
+	
+	int** coordinates;	// Initialize 2d array 
+	coordinates = (int**) malloc((X_1-X_0)/X_STEP * sizeof(int*));	// Allocate dynamic storage (X-Value), type = int* (address)
+	
+	
+	if (coordinates == NULL) {	// Exit programm if allocation failed 
+		printf("Allocation failed.\n");
 
+		return 1;
+	}
+	
+
+	for (int i = 0; i < ((X_1 - X_0) / X_STEP); i++) {
+		coordinates[i] = (int*) malloc(((Y_1 - Y_0) / Y_STEP) * sizeof(int*));	// Allocate dynamic storage (Y-Value) [ERROR] ?
+		
+	
+		if (coordinates[i] == NULL) {
+			printf("Allocation failed.\n");
+
+			for (int j = i - 1; j >= 0; j--) {	// Free storage (Y-Values)
+				free(coordinates[j]);	
+			}
+			free(coordinates);	// Free storage (X-Values)
+			return 1;
+		}
+		
+	}
+
+	// [DEBUG] Simple test:
+	for (int i = 0; i < ((X_1 - X_0) / X_STEP); i++) {
+		for (int j = 0; j < ((Y_1 - Y_0) / Y_STEP); j++) {
+			coordinates[i][j] = i + j;
+		}
+	}
+	for (int i = 0; i < ((X_1 - X_0) / X_STEP); i++) {
+		for (int j = 0; j < ((Y_1 - Y_0) / Y_STEP); j++) {
+			printf("%d ", coordinates[i][j]);
+		}
+		printf("\n");
+	}
+
+
+
+	// Release storage at the end:
+	for (int i = 0; i < ((X_1 - X_0) / X_STEP); i++) {	// Free storage (Y-Values)
+		free(coordinates[i]);
+	}
+	free(coordinates);	// Free storage (X-Values)
 
 	return 0;
 }
