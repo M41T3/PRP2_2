1116148691b75d1daa0d5507bc5895a3f8b63845
PC-MALTE\malte <maltemueller1104@gmail.com>
2018-11-01 17:26:02 +0100

    realloc in function - does not work yet

--- Messwerte/Messwerte/messwerte.c
+++ Messwerte/Messwerte/messwerte.c
@@ -10,13 +10,17 @@ Date: 31.10.2018
 
 float **allocate_2d(int n_days);
 float  *allocate_1d(int n_days);
-void scan_data(float **temperature, float *air_pressure, int n_days);
+void scan_data(float **temperature, float *air_pressure, int from, int to);
 float mean_temperature(float **temperature, int n_days);
 float mean_air_pressure(float *air_pressure, int n_days);
+float **add_temperature(float **temperature, int new_size);
+float *add_air_pressure(float *air_pressure, int new_size);
 
 int main(void) {
 
 	int n_days;
+	int new_days = 0;
+	int from = 0;
 
 	printf("F%cr wieviele Tage sollen die Messdaten erfasst werden? ", 129);
 	scanf_s(" %d", &n_days);
@@ -40,7 +44,23 @@ int main(void) {
 
 	do {
 
-		scan_data(temperature, air_pressure, n_days);
+		scan_data(temperature, air_pressure, from, n_days);
+
+		//[DEBUG]vvvvvvvv
+		printf("\n[DEBUG] Print out Temperature-2dArray:\n\n");
+		for (int i = 0; i < n_days; i++) {
+			for (int j = 0; j < N; j++) {
+				printf("%f ", temperature[i][j]);
+			}
+			printf("\n");
+		}
+
+		printf("\n[DEBUG] Print out Pressure-1dArray:\n\n");
+		for (int i = 0; i < n_days; i++) {
+			printf("%f ", air_pressure[i]);
+		}
+		printf("\n");
+		//^^^^^^^^
 
 		// Calc mean:
 		printf("\n--> Durchschnittswert der Temperaturen: %.2f\n", mean_temperature(temperature, n_days));
@@ -50,8 +70,27 @@ int main(void) {
 		scanf_s(" %c", &rep_flag);
 
 		if (rep_flag == 'j') {
-			// realloc
-			// ask for new data
+			// realloc:
+			printf("Wieviele neue Tage? \n");
+			scanf_s(" %d", &new_days);
+
+			from = n_days;
+			n_days = n_days + new_days;
+
+			air_pressure = add_air_pressure(air_pressure, n_days);
+			if (air_pressure == NULL) {
+				printf("Allocation failed: air_pressure == NULL!\n");
+
+				return 1;
+			}
+			
+			temperature = add_temperature(temperature, n_days);
+			if (temperature == NULL) {
+				printf("Allocation failed: temperature == NULL!\n");
+			
+				return 1;
+			}
+			
 		}
 
 	} while (rep_flag == 'j');
@@ -60,6 +99,7 @@ int main(void) {
 	for (int i = 0; i < n_days; i++) {	// Free storage (Y-Values)
 		free(temperature[i]);
 	}
+
 	free(temperature);	// Free storage (X-Values)
 	free(air_pressure);
 
@@ -131,11 +171,11 @@ Parameters:
 Returns:
 	void.
 */
-void scan_data(float **temperature, float *air_pressure, int n_days) {
+void scan_data(float **temperature, float *air_pressure, int from, int to) {
 
 	printf("* Einlesen der Messwerte f%cr die einzelchen Tage *\n", 129);
 
-	for (int i = 0; i < n_days; i++) {
+	for (int i = from; i < to; i++) {
 		printf("Tag %d:\n", i+1);
 		for (int j = 0; j < N; j++) {
 			printf("\tTemperatur %d: ", j+1);
@@ -178,4 +218,48 @@ float mean_air_pressure(float *air_pressure, int n_days) {
 		i += air_pressure[k];
 	}
 	return i / n_days;
+}
+
+/**************************************************************************************************
+temp Realloc-fnc.
+*/
+float **add_temperature(float **temperature, int new_size) {
+	
+	float **new_temp = (float**)realloc(temperature, new_size * sizeof(float*));
+	
+	if (new_temp == NULL) {	// Exit programm if allocation failed 
+		return NULL;
+	}
+
+	for (int i = 0; i < new_size; i++) {
+		new_temp[i] = (float*)malloc(N * sizeof(float));
+		
+		printf("%d\n", i); // [DEBUG]
+
+		if (new_temp[i] == NULL) {
+			printf("fail at %d\n", i); // [DEBUG]
+			for (int j = i - 1; j >= 0; j--) {	// Free storage (Y-Values)
+				free(new_temp[j]);
+			}
+			free(new_temp);	// Free storage (X-Values)
+			return NULL;
+		}
+
+	}
+
+	return new_temp;
+}
+
+/**************************************************************************************************
+air Realloc-fnc.
+*/
+
+float *add_air_pressure(float *air_pressure, int new_size) {
+	float *new_air_pr = (float*)realloc(air_pressure, new_size);
+
+	if (new_air_pr == NULL) {	// Exit programm if allocation failed 
+		return NULL;
+	}
+
+	return new_air_pr;
 }
